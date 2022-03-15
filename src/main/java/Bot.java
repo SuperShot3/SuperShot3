449b82f5c1434ae7a53248f9fb64f190e42640b4
Kons123 <polovnikov.k.v@gmail.com>
2022-03-15 14:05:42 +0500

    My first commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,41 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Bot extends TelegramLongPollingBot {
+    SendMessage sendMessage = new SendMessage();
+    Bot bot;
+    @Override
+    public String getBotUsername() {
+        return "@letsnewbot_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5076632172:AAEsozThZiwZKSspYPtky6JX1r3qCdgEadQ";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
+        sendMessage.setText("hi");
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class Main{
+        public static void main(String[] args) throws TelegramApiException {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new Bot());
+        }
+
+    }
+
+
+}
