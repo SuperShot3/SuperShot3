fe9ab2be2d2cc8bbe93f69a475cf7850f4d339f1
Kons123 <polovnikov.k.v@gmail.com>
2022-03-15 15:01:02 +0500

    MycommitFromIdea

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -cp target/classes:target/dependency/* com.example.Bot
+web: java $JAVA_OPTS -cp target/classes:target/dependency/* com.example.com.example.Bot
--- src/main/java/com.example/Bot.java
+++ src/main/java/com.example/Bot.java
@@ -1,3 +1,5 @@
+package com.example;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
